de66ff885571063fcdcb44698b8f0f6c78b28183
김수빈 <westuse38@naver.com>
2023-11-26 14:53:15 +0900

    [feat] 옵션탭 추가

--- lib/login/login_api.dart
+++ lib/login/login_api.dart
@@ -1,28 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-final FirebaseAuth _auth = FirebaseAuth.instance;
+final FirebaseAuth auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn(
-    clientId: "592989854108-lttqfekciv3pp1tvvji5oscvdojf177g.apps.googleusercontent.com"
-);
+    clientId:
+        "592989854108-lttqfekciv3pp1tvvji5oscvdojf177g.apps.googleusercontent.com");
 
 Future<User?> signInWithGoogle() async {
   final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
   if (googleSignInAccount != null) {
-    final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
     final AuthCredential credential = GoogleAuthProvider.credential(
       accessToken: googleSignInAuthentication.accessToken,
       idToken: googleSignInAuthentication.idToken,
     );
 
-    final UserCredential authResult = await _auth.signInWithCredential(credential);
+    final UserCredential authResult =
+        await auth.signInWithCredential(credential);
     final User? user = authResult.user;
 
     if (user != null) {
       assert(!user.isAnonymous);
       assert(await user.getIdToken() != null);
 
-      final User? currentUser = _auth.currentUser;
+      final User? currentUser = auth.currentUser;
       assert(user.uid == currentUser?.uid);
 
       print('Google SignIn Succeeded: ${user.uid}');
